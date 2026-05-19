4db6df21ee1de4bee51a01aed423f9d4a3bd4637
mohitpanjwani1303-pos <mohitpanjwani1303@gmail.com>
2026-05-19 13:14:07 +0530

    Add FileManager.h for account management functions

---
+++ FileManager.h
@@ -0,0 +1,10 @@
+#ifndef FILEMANAGER_H
+#define FILEMANAGER_H
+
+#include "BankAccount.h"
+
+void writeAccount();
+void displayAccount(int accNo);
+void modifyAccount(int accNo, int option);
+
+#endif
